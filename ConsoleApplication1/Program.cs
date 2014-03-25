06a27b80971c2df8c6e9bece5d2af2787c89b29d
sytha <sytha38@gmail.com>
2014-03-25 16:38:41 +0100

    Tous mon projet

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LOTO
+{
+    class Loto
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------------------------\n");
+            Console.WriteLine("----------*    ****       *     **** ***** ****----------");
+            Console.WriteLine("----------*    ***        *     *  *   *   *  *----------");
+            Console.WriteLine("----------**** ****       ****  ****   *   ****----------\n");
+            DateTime date = DateTime.Now;
+            Console.WriteLine("----------Tirage du "+date.Day+"----------\n");
+            int[] tabloto = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+
+            }
+        }
+    }
+}
