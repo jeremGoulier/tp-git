6aa231cf71e01d4f0b89f30489ded3c21be77fb6
jeremGoulier <jeremy-goulier@hotmail.fr>
2014-03-25 17:08:32 +0100

    Update Program.cs

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,20 +6,30 @@ using System.Text;
 namespace LOTO
 {
     class Loto
-    {
         static void Main(string[] args)
         {
-            Console.WriteLine("---------------------------------------------------------\n");
-            Console.WriteLine("----------*    ****       *     **** ***** ****----------");
-            Console.WriteLine("----------*    ***        *     *  *   *   *  *----------");
-            Console.WriteLine("----------**** ****       ****  ****   *   ****----------\n");
-            DateTime date = DateTime.Now;
-            Console.WriteLine("----------Tirage du "+date.Day+"----------\n");
-            int[] tabloto = new int[6];
+            int[] tabLoto = new int[6];
+            int verif;
+            Random toto = new Random();
+            DateTime dateTirage = DateTime.Now;
+
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("*     ****            *     ****  *****  ****    ");
+            Console.WriteLine("*     **              *     *  *    *    *  *    ");
+            Console.WriteLine("****  ***             ****  ****    *    ****    ");
+            Console.WriteLine("\n--Nous sommes le : " + dateTirage + "----------");
+
             for (int i = 0; i < 6; i++)
             {
-
+                verif = toto.Next(149);
+                if(tabLoto.Contains(verif)){
+                    i = i - 1;
+                }else{
+                    tabLoto[i] = toto.Next(1,49);
+                }
             }
+            Console.WriteLine("\n" + tabLoto[0] + "  " + tabLoto[1] + "  " + tabLoto[2] + "  " + tabLoto[3] + "  " + tabLoto[4] + "  " + tabLoto[5]);
+            Console.ReadKey(true);
         }
     }
 }
